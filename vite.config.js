fd8ee92ce6024072eadf56a008ea3236297f83cf
Serianny Moreno <serianny@example.com>
2025-11-21 22:34:43 -0500

    Fix: actualización de vite.config.js para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/gametracker-frontend/",
   plugins: [react()],
-});
+  base: process.env.NODE_ENV === 'production'
+    ? '/gametracker-frontend/'
+    : '/',
+})
